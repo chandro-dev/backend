a65db85942e10583b66b7b547e3c05b911066205
Jose Larrada <jalarrada@unicesar.edu.co>
2024-03-05 19:50:07 -0500

    Entidad Personas y paquetes

---
+++ src/main/java/ara/main/Entity/Person.java
@@ -0,0 +1,16 @@
+package ara.main.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    private String id;
+}
