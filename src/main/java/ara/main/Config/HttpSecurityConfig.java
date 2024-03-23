3a495d0e9bb85dc3b0b43309db6d647d0c867eb3
JoseLarrada <jalarrada@unicesar.edu.co>
2024-03-23 10:26:35 -0500

    Se agregó la documentacion de swagger al proyecto

--- src/main/java/ara/main/Config/HttpSecurityConfig.java
+++ src/main/java/ara/main/Config/HttpSecurityConfig.java
@@ -19,6 +19,17 @@ public class HttpSecurityConfig {
     private AuthenticationProvider authenticationProvider;
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
+    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"};
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -33,6 +44,7 @@ public class HttpSecurityConfig {
                     authConfig.requestMatchers(HttpMethod.GET,"/personas/**").permitAll();
                     authConfig.requestMatchers(HttpMethod.GET,"/auth/public-access").permitAll();
                     authConfig.requestMatchers("/error").permitAll();
+                    authConfig.requestMatchers(WHITE_LIST_URL).permitAll();
 
                     //Metodos privados
                     authConfig.requestMatchers(HttpMethod.GET,"/products").hasAnyAuthority(Permission.SEE_ALL_PRODUCT.name()); //Solo pueden acceder las personas que cumplan con ese rol
---
+++ src/main/java/ara/main/Config/OpenApiConfig.java
@@ -0,0 +1,56 @@
+package ara.main.Config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Markersproject SA",
+                        email = "Markersproject@outlook.com",
+                        url = "https://localhos:8080/home"
+                ),
+                description = "Documentacion de la apí de proyecto de tiendas",
+                title = "OpenApi specification - Markersproject",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terminos y servicios"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://Jetcheck.com/home"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+
+public class OpenApiConfig {
+
+}
