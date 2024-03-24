e5b8cc6c128276b1f23b4223716dd09055d522f8
Luis alejandro Raptor <alejo_carretero@hotmail.com>
2024-03-24 09:00:04 -0500

    find products by brand

--- src/main/java/ara/main/Controller/BrandController.java
+++ src/main/java/ara/main/Controller/BrandController.java
@@ -1,11 +1,13 @@
 package ara.main.Controller;
 
 import ara.main.Entity.Brand;
+import ara.main.Entity.Product;
 import ara.main.Entity.persons;
 import ara.main.Service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,9 @@ public class BrandController {
     @GetMapping()
     public ResponseEntity<List<Brand>> getAll(){
         return  brandService.getAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Product>> getProductbyBrand(@PathVariable int id) {
+        return brandService.getToCBrand(id);
     }
 }
