96b4f7677e4a2f31b0f176c88090c7292578fc3e
Jose Larrada <jalarrada@unicesar.edu.co>
2024-04-18 13:02:31 -0500

    Filtro de categorias

--- src/main/java/ara/main/Dto/PersonsDto.java
+++ src/main/java/ara/main/Dto/PersonsDto.java
@@ -1,4 +1,18 @@
 package ara.main.Dto;
 
+import ara.main.Dto.util.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class PersonsDto {
+    private String identification;
+    private String name;
+    private String lastname;
+    private String email;
 }
