c0f890ec9b35190132a7d52bd2dde4c6b24767df
Jose Larrada <jalarrada@unicesar.edu.co>
2024-05-04 15:45:35 -0500

    cambio order details

--- src/main/java/ara/main/Controller/OrdersDetailsController.java
+++ src/main/java/ara/main/Controller/OrdersDetailsController.java
@@ -1,13 +1,11 @@
 package ara.main.Controller;
 
 import ara.main.Dto.OrderDto;
+import ara.main.Entity.OrderDetails;
 import ara.main.Service.OrderDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
